4ebdbc156ee287f461bbaeec02efee38e82b5699
awadheshkverma <awadhbioinfo@gmail.com>
2023-03-23 15:09:39 +0530

    01-hello-r.R

---
+++ 01-hello-r.R
@@ -0,0 +1,20 @@
+my_var <- c( 
+	    a ='apple',
+	    b ='boy', 
+	    c ='cat', 
+	    d ='desktop'
+	    )
+my_var[3]
+
+my_var['c']
+
+my-var[3]
+
+my_var['c']
+
+my_var[c(3,2,1,4)]
+
+my_var[c('c','b','a','d')]
+
+
+
